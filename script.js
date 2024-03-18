8ecae741adf2254f5b4107442b0b8fea7efae1f2
aaaveee <95289436+aaaveee@users.noreply.github.com>
2024-03-18 13:07:01 +0200

    calculator working
    - finished programming delete and computation operations. the calculator should be working as intended
    - modified the styling: changed font and background gradient

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class Laskin {
+class Calculator {
     constructor(previousOperaTextElement, currentOperaTextElement) {
         this.previousOperaTextElement = previousOperaTextElement
         this.currentOperaTextElement = currentOperaTextElement
@@ -12,7 +12,7 @@ class Laskin {
     }
 
     delete() { // delete operation
-
+        this.currentOpera = this.currentOpera.toString().slice(0, -1) // changes the current operand to a string and slices the last digit from it
     }
 
     appendNumber(number) { // when user clicks a number, adds it to the screen
@@ -30,13 +30,58 @@ class Laskin {
         this.currentOpera = ''
     }
 
-    compute() { //compute value
-
+    compute() { // compute value
+        let computation // result of compute function
+        const prev = parseFloat(this.previousOpera) // number version of previous operand
+        const current = parseFloat(this.currentOpera) // number version of current operand
+        if (isNaN(prev) || isNaN(current)) return // if no previous or current value, cancel function
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '/':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOpera = computation
+        this.operation = undefined
+        this.previousOpera = ''
     }
 
+    getDisplayNumber(number) { // formats commas to large numbers
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1] // handels decimal integers
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+          integerDisplay = ''
+        } else {
+          integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`
+        } else {
+          return integerDisplay
+        }
+      }
+
     updateDisplay() { // update values inside the output
-        this.currentOperaTextElement.innerText = this.currentOpera
-        this.previousOperaTextElement.innerText = this.previousOpera
+        this.currentOperaTextElement.innerText = 
+            this.getDisplayNumber(this.currentOpera)
+        if (this.operation != null) {
+            this.previousOperaTextElement.innerText = 
+                `${this.getDisplayNumber(this.previousOpera)} ${this.operation}`
+        } else {
+            this.previousOperaTextElement.innerText = ''
+        } 
     }
 }
 
@@ -49,18 +94,33 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperaTextElement = document.querySelector('[data-previous-opera]')
 const currentOperaTextElement = document.querySelector('[data-current-opera]')
 
-const laskin = new Laskin(previousOperaTextElement, currentOperaTextElement)
+const calculator = new Calculator(previousOperaTextElement, currentOperaTextElement)
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        laskin.appendNumber(button.innerText)
-        laskin.updateDisplay()
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
     })
 })
 
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
-        laskin.chooseOperation(button.innerText)
-        laskin.updateDisplay()
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
     })
 })
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+  })
+
+allClearButton.addEventListener('click', button => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+  })
